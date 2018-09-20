f25bb67f2220de302272f737bce1dce577367b0a
Anil Chalisey <anilchalisey@gmail.com>
2018-09-20 14:25:42 +0100

    added file.move and write.df functions

---
+++ R/writedf.R
@@ -0,0 +1,18 @@
+#' Write a data.frame to file with sensible defaults
+#'
+#' @description This is simply a wrapper around write.table with the
+#' settings I use most often: quote = FALSE, sep = "\t", row.names = FALSE,
+#' and col.names = TRUE
+#'
+#' @inheritParams utils::write.table
+#'
+#'
+#' @seealso \code{\link{utils}{write.table}}
+#'
+#' @export
+
+write.df <- function(x, file = "", quote = FALSE, sep = "\t",
+                     row.names = FALSE, col.names = TRUE) {
+  write.table(x = x, file = file, quote = quote, sep = sep,
+              row.names = row.names, col.names = col.names)
+}
